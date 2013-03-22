2509d781a98c81da4c82ab11bd66d94254736cf1
Richard Boer <richardboer@gmail.com>
2013-03-22 13:01:40 +0100

    Initial base

---
+++ includes/functions.inc.php
@@ -0,0 +1,12 @@
+<?php
+//File containing the commonly used functions.
+
+//MySQL connect function
+function connect_mysql()
+    {
+        include_once('config.inc.php');
+        $link = mysql_connect($mysql_host.':'.$mysql_port, $mysql_user,$mysql_password);
+        mysql_select_db($mysql_db, $link) or die('Could not select database.');
+        return $link;
+    }
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+//Home page used to load the multiple pages, and this file will contain the style of the website.
+
+//Loading the multiple incldues
+include_once('config.inc.php');
+include_once('includes/functions.inc.php');
+connect_mysql();
+?>
